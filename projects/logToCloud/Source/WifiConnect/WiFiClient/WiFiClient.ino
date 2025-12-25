485d02ef2fc1c94c1c5609f350565760261ab4dd
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-12-25 09:25:15 +0300

    update the file for dht sensor and lcd 20x4 lcd screen

--- projects/logToCloud/Source/WifiConnect/WiFiClient/WiFiClient.ino
+++ projects/logToCloud/Source/WifiConnect/WiFiClient/WiFiClient.ino
@@ -1,47 +1,38 @@
-/*
-    Go to thingspeak.com and create an account if you don't have one already.
-    After logging in, click on the "New Channel" button to create a new channel for your data. This is where your data will be stored and displayed.
-    Fill in the Name, Description, and other fields for your channel as desired, then click the "Save Channel" button.
-    Take note of the "Write API Key" located in the "API keys" tab, this is the key you will use to send data to your channel.
-    Replace the channelID from tab "Channel Settings" and privateKey with "Read API Keys" from "API Keys" tab.
-    Replace the host variable with the thingspeak server hostname "api.thingspeak.com"
-    Upload the sketch to your ESP32 board and make sure that the board is connected to the internet. The ESP32 should now send data to your Thingspeak channel at the intervals specified by the loop function.
-    Go to the channel view page on thingspeak and check the "Field1" for the new incoming data.
-    You can use the data visualization and analysis tools provided by Thingspeak to display and process your data in various ways.
-    Please note, that Thingspeak accepts only integer values.
-
-    You can later check the values at https://thingspeak.com/channels/2005329
-    Please note that this public channel can be accessed by anyone and it is possible that more people will write their values.
- */
-
 #include <WiFi.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
-const char *ssid = "Wokwi-GUEST";          // Change this to your WiFi SSID
-const char *password = "";  // Change this to your WiFi password
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
-const char *host = "api.thingspeak.com";        // This should not be changed
-const int httpPort = 80;                        // This should not be changed
-const String channelID = "2005329";             // Change this to your channel ID
-const String writeApiKey = "V6YOTILH9I7D51F9";  // Change this to your Write API key
-const String readApiKey = "34W6LGLIFXD56MPM";   // Change this to your Read API key
+#define LCD_ADDR 0x27
+LiquidCrystal_I2C lcd(LCD_ADDR, 20, 4);
 
-// The default example accepts one data filed named "field1"
-// For your own server you can ofcourse create more of them.
-int field1 = 0;
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
 
-int numberOfResults = 3;  // Number of results to be read
-int fieldNumber = 1;      // Field number which will be read out
+const char* host = "api.thingspeak.com";
+const int httpPort = 80;
+const String writeApiKey = "KZ5PU7BCML4U7MW2";
+
+WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
 
-  // We start by connecting to a WiFi network
+  dht.begin();
 
-  Serial.println();
-  Serial.println("******************************************************");
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+  delay(1000);
 
+  Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -49,57 +40,97 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println("\nWiFi connected");
+  Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("WiFi Connected!");
 }
 
-void readResponse(NetworkClient *client) {
+void readResponse(WiFiClient* client) {
   unsigned long timeout = millis();
   while (client->available() == 0) {
     if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
+      Serial.println(">>> Client Timeout!");
       client->stop();
       return;
     }
   }
 
-  // Read all the lines of the reply from server and print them to Serial
   while (client->available()) {
     String line = client->readStringUntil('\r');
     Serial.print(line);
   }
-
-  Serial.printf("\nClosing connection\n\n");
+  Serial.println("\nClosing connection\n");
 }
 
 void loop() {
-  NetworkClient client;
-  String footer = String(" HTTP/1.1\r\n") + "Host: " + String(host) + "\r\n" + "Connection: close\r\n\r\n";
+  float temperature = dht.readTemperature(); // Celsius
+  float humidity = dht.readHumidity();
 
-  // WRITE --------------------------------------------------------------------------------------------
-  if (!client.connect(host, httpPort)) {
-    return;
+  if (isnan(temperature) || isnan(humidity)) {
+    temperature = random(20, 35);
+    humidity = random(40, 80);
   }
 
-  client.print("GET /update?api_key=" + writeApiKey + "&field1=" + field1 + footer);
-  readResponse(&client);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print(" C");
 
-  // READ --------------------------------------------------------------------------------------------
+  lcd.setCursor(0, 1);
+  lcd.print("Hum:  ");
+  lcd.print(humidity);
+  lcd.print(" %");
 
-  String readRequest = "GET /channels/" + channelID + "/fields/" + fieldNumber + ".json?results=" + numberOfResults + " HTTP/1.1\r\n" + "Host: " + host + "\r\n"
-                       + "Connection: close\r\n\r\n";
+  if (client.connect(host, httpPort)) {
+    String writeURL = "/update?api_key=" + writeApiKey +
+                      "&field1=" + String(int(temperature)) +
+                      "&field2=" + String(int(humidity));
 
-  if (!client.connect(host, httpPort)) {
-    return;
+    client.print(String("GET ") + writeURL + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    Serial.print("Sending Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" °C, Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+
+    unsigned long timeout = millis();
+    bool success = false;
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println(">>> Client Timeout!");
+        client.stop();
+        break;
+      }
+    }
+
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+      if (line.indexOf("200") != -1) success = true;
+    }
+    client.stop();
+
+    lcd.setCursor(0, 2);
+    if (success) {
+      Serial.println("✅ Data successfully sent to ThingSpeak!");
+      lcd.print("Data sent ✅");
+    } else {
+      Serial.println("Failed to send data.");
+      lcd.print("Data failed ❌");
+    }
+  } else {
+    Serial.println("Connection failed to ThingSpeak");
+    lcd.setCursor(0, 2);
+    lcd.print("No Connection ❌");
   }
 
-  client.print(readRequest);
-  readResponse(&client);
-
-  // -------------------------------------------------------------------------------------------------
-
-  ++field1;
-  delay(10000);
+  delay(10000); 
 }
